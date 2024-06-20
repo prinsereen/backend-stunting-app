6b12412e0d9a35631cea67f8c3f16925b36493cd
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2024-06-20 17:30:16 +0700

    go back to local

--- config/Database.js
+++ config/Database.js
@@ -1,15 +1,8 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
+import {Sequelize} from "sequelize";
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
+const db = new Sequelize('db_ium', 'root', '', {
+    host: "localhost",
     dialect: "mysql",
-  }
-);
+})
 
 export default db;
