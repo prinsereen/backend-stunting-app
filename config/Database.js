dec142a6e268de97809a7f21ff9bb8f7f783e0bd
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2024-06-20 15:30:52 +0700

    add to prod

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,14 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const db = new Sequelize('db_ium', 'root', '', {
-    host: "localhost",
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
     dialect: "mysql",
-})
+  }
+);
 
 export default db;
